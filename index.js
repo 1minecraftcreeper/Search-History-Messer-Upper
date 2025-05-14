b02a69ef196534001a486473916703e5ba817cf1
1minecraftcreeper <24815@verona.k12.wi.us>
2025-05-14 09:04:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 function addTerm() {
-
+  term = document.getElementById("searchTerm");
 }
 function downloadHtml() {
-
+  var blob = new Blob([document.getElementById("outputText").innerText], { type: "html" });
+  var fileURL = URL.createObjectURL(blob);
+  var downloadLink = document.createElement('a');
+  downloadLink.href = fileURL;
+  alert(fileURL);
+  downloadLink.download = "example.html";
+  downloadLink.target = "download"
+  downloadLink.innerText = "Download as html"
+  document.getElementById("div2").appendChild(downloadLink);
+  downloadLink.click();
+  //window.location.href=fileURL;
+  //window.open(fileURL);
 }
